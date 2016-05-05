50cda123add08783e8d460d894557b7932188ab4
arsylum <arsylum@users.noreply.github.com>
2016-05-05 20:08:12 +0200

    make produce

---
+++ src/js/build.options.js
@@ -0,0 +1,19 @@
+({
+    baseUrl: ".",
+    mainConfigFile: 'main.js',
+    wrapShim: true,
+    paths: {
+        'jquery': "empty:",
+        "jquery-ui": "empty:",
+        "bootstrap": "empty:",
+
+		"angular": "empty:",
+		"ngAnimate": "empty:",
+		"ngRoute": "empty:",
+		"ngTranslate": "empty:",
+		"ngComplete": "empty:"
+    },
+    name: "main",
+    out: "dist.min.js",
+
+})
