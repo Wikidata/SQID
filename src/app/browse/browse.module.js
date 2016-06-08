343e718537951ce8cb047a934dc9dcd480cb9d42
arsylum <georg.wild@tu-dresden.de>
2016-06-08 02:59:05 +0200

    some first cleaning effort

--- src/app/browse/browse.module.js
+++ src/app/browse/browse.module.js
@@ -4,13 +4,14 @@ define([
 	'i18n/i18n.service',
 	'util/util.module',
 	'util/pagination.controller',
-	'data/data.module'
+	'data/data.module',
+	'ngComplete'
 ], function() {
 ///////////////////////////////////////
 
 angular.module('browse', [
 	'ngRoute', 
-	'data', 'util', 'i18n'
+	'data', 'util', 'i18n', 'angucomplete-alt'
 ]);
 
 
--- src/app/sqid.module.js
+++ src/app/sqid.module.js
@@ -1,60 +1,25 @@
 'use strict'; // indicate that code is executed in strict mode
 
-define([ // module definition dependencies
-	'ngAnimate', 
-	'ngRoute',
-	'ngCookies',
-	//'ngTranslate', // is (has to be?) loaded explicitly in main.js
-	//'ngComplete',
-	// 'util/util',
-	'ui-boostrap-tpls', // implicit bootstrap
-	'i18n/i18n.module',
-	//'core/core.module',
-	// 'query/query'
-	// 'i18n/i18n.service',
-	// 'browse/browse.module',
-	// 'browse/arguments.service',
-	// 'browse/table.controller',
-	// 'ngCookies',				//
-	// 'ngTranslate-core',			// cannot for the life of me sort out the 
-	// 'ngTranslate-loader',		// dependency in a way that the bundle will
-	// 'ngTranslate-storage-cook',	// execute in the right order when optimized
-	// 'ngTranslate-storage-loc',	// unless loading in this order explicitly
-
+define([ // sqid application wrapper module
+	'layout/layout.directives',
+	
 	// config blocks must register before bootstrap
 	'core/core.config',
 	'i18n/translate.config',
-	'layout/layout.module',
-	'layout/layout.directives',
 	'meta/meta.config',
 	'browse/browse.config',
 	'view/view.config',
 	'query/sparqly.config'
-
 ], function() {
 
 $("[data-toggle=popover]").popover({html:true}); // <-- is this in use?
 
-var classBrowser = angular.module('sqid',[
-	// 'ngCookies', //'ngAnimate', 'ngRoute', 'ngCookies',  'ui.bootstrap', 'pascalprecht.translate', 'angucomplete-alt', 
-	'core', 'data', 'i18n', 'util',//,'queryInterface'
-	'browse', 'layout', 'view', 'sparqly'
-]); //.controller('Foo', ['$translate', function(i18n) {
-// 	console.log(i18n);
-// 	angular.transalat = i18n;
-// } ]);
-// require(['i18n/translate.config']);
 
-// angular.module('core').config(['$routeProvider', function($routeProvider) {
-// 		$routeProvider
-			
-// 			.when('/browse', { templateUrl: 'views/browseData.html' });
-// 			// .when('/datatypes', { templateUrl: 'views/datatypes.html' })
-// 			//.when('/about', { templateUrl: 'app/core/about.html' })
-// 			// .when('/status', { templateUrl: 'views/status.html' })
-// 			// .when('/view', { templateUrl: 'views/view.html' })
-// 			// .when('/query', { templateUrl: 'views/queryview.html'})
-// 			//.otherwise({redirectTo: '/'});
-// 	}]);
+angular.module('sqid',[						// the application wrapper module
+
+	'data', 'i18n', 'util', 'layout',				// shared modules
+	
+	'core', 'meta', 'browse', 'view', 'sparqly'		// feature / content modules
+]); 
 
 return {};}); // module definition end
