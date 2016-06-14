ce3661e9c559b6f8e3f712200365358a9125fbbd
arsylum <georg.wild@tu-dresden.de>
2016-06-14 15:06:51 +0200

    pagination nav index range caption directive

--- src/app/util/pagination.directives.js
+++ src/app/util/pagination.directives.js
@@ -7,9 +7,9 @@ define([
 angular.module('util')
 
 /////////////////////////////////////////////////
-///// widget for selecting number of entities //
-//// 								per page //
-/// use like								//
+///// widget for selecting number of entities //	
+//// 								per page //		*  customize proposed options like
+/// use like								//	    * $scope.pagination.tableSizeOpts = [15,25,50,100];
 // <tableSizeSelector></tableSizeSelector> //
 ////////////////////////////////////////////
 .directive('tableSizeSelector', function() {
@@ -33,6 +33,29 @@ angular.module('util')
 		restrict: 'A'
 	};
 
+}])
+
+//////////////////////////////////////////////////////////
+//// 	  displays active index range of current page  //
+/// use like										  //
+// <pagination-index-range></pagination-index-range> //
+// ///////////////////////////////////////////////////
+.directive('paginationIndexRange', [function(){
+	return {
+		controller: ['$scope', function($scope) {
+		 	var pgnt = $scope.$parent.pagination;
+		 	$scope.getFrom = function() { return '<span class="info-badge">' + pgnt.fromItem; }
+		 	$scope.getTo = function() { return pgnt.toItem + '</span>';	}
+		 	$scope.getTotal = function() { return '<span class="info-badge">' + pgnt.numItems + '</span>'; 	}
+
+		}],
+		template: '<div class="pagination-nav-caption" ' + 
+				'translate="PAGINATION.ACTIVE_INDEX_CAPTION_TEXT" ' +
+				'translate-values=\'{ to: getTo(), from: getFrom(), total: getTotal() }\' ' + 
+				'translate-compile> ' +
+			'</div>',
+		restrict: 'E'
+	};
 }]);
 
 
