4c15b2c093eafe7149db2b3086e9787568302026
arsylum <georg.wild@tu-dresden.de>
2016-06-14 12:37:52 +0200

    pagination directive

--- src/app/util/pagination.directives.js
+++ src/app/util/pagination.directives.js
@@ -4,12 +4,36 @@ define([
 ], function() {
 ///////////////////////////////////////
 
-angular.module('util').directive('tableSizeSelector', function() {
+angular.module('util')
+
+/////////////////////////////////////////////////
+///// widget for selecting number of entities //
+//// 								per page //
+/// use like								//
+// <tableSizeSelector></tableSizeSelector> //
+////////////////////////////////////////////
+.directive('tableSizeSelector', function() {
 	return {
 		templateUrl: 'app/util/pagination.tableSizeSelector.tpl.html',
 		restrict: 'E'
 	};
-});
+})
+
+///////////////////////////////////////////////////
+//// 						pagination nav bar  //
+/// use like								   //
+// <nav pagination="$scope.pagination"></nav> //
+///////////////////////////////////////////////
+.directive('pagination', [function() {
+	return {
+		scope: {
+			pgnt: '<pagination'
+		},
+		templateUrl: 'app/util/pagination.pageNav.tpl.html',
+		restrict: 'A'
+	};
+
+}]);
 
 
 return {}; }); // module definition end
