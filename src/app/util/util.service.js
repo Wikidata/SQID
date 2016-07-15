9906dfca87f08c86320e73fd64ae9575ef437426
Markus Kroetzsch <markus@semantic-mediawiki.org>
2016-07-15 17:18:29 +0200

    fixed indenting

--- src/app/util/util.service.js
+++ src/app/util/util.service.js
@@ -36,7 +36,7 @@ angular.module('util').factory('util', ['$http', '$q', function($http, $q) {
 			return $q.reject(response.data);
 		});
 	}
-	
+
 	var getIdFromUri = function(uri) {
 		if ( uri.substring(0, "http://www.wikidata.org/entity/".length) === "http://www.wikidata.org/entity/" ) {
 			return uri.substring("http://www.wikidata.org/entity/".length, uri.length);
@@ -46,22 +46,22 @@ angular.module('util').factory('util', ['$http', '$q', function($http, $q) {
 	}
 
 	var cloneObject = function(obj) {
-	    if (obj === null || typeof obj !== 'object') {
-	        return obj;
-	    }
-	 
-	    var temp = obj.constructor(); // give temp the original obj's constructor
-	    for (var key in obj) {
-	        temp[key] = cloneObject(obj[key]);
-	    }
-	 
-	    return temp;
+		if (obj === null || typeof obj !== 'object') {
+			return obj;
+		}
+
+		var temp = obj.constructor(); // give temp the original obj's constructor
+		for (var key in obj) {
+			temp[key] = cloneObject(obj[key]);
+		}
+
+		return temp;
 	};
 
 	var unionArrays = function(a1, a2){
 		var unique = [];
 		$.each(a1.concat(a2), function(i, el){
-    		if($.inArray(el, unique) === -1){
+			if($.inArray(el, unique) === -1){
 				unique.push(el);
 			}
 		});
@@ -83,35 +83,35 @@ angular.module('util').factory('util', ['$http', '$q', function($http, $q) {
 	}
 
 	var getSortComparator = function(criteria, direction){
-      return function(data){
-        return function(a, b){
-          var x = a;
-          var y = b;
-          var a = data[a][criteria];
-          var b = data[b][criteria];
-          if (a == b){          	
-          	return 0;
-          }
-          if ((b == undefined) || (a > b)){
-            return 1 * direction;
-          }
-          if ((a == undefined) || (a < b)){
-            return (-1) * direction;
-          }
-          return 0;
-        };
-      }
-    }
+		return function(data){
+			return function(a, b){
+				var x = a;
+				var y = b;
+				var a = data[a][criteria];
+				var b = data[b][criteria];
+				if (a == b){
+					return 0;
+				}
+				if ((b == undefined) || (a > b)){
+					return 1 * direction;
+				}
+				if ((a == undefined) || (a < b)){
+					return (-1) * direction;
+				}
+				return 0;
+			};
+		}
+	}
 
-    var reverseDeepCopy = function(array){
-    	var newarray = [];
+	var reverseDeepCopy = function(array){
+		var newarray = [];
 		var j=0;
 		for (var i=array.length-1; i >= 0; i--){
 			newarray[j] = array[i];
 			j++;
 		}
 		return newarray;
-    }
+	}
 
 	return {
 		httpRequest: httpRequest,
