f1e5dd342700b554d1794fb55e99296c1aa18128
Maximilian Marx <maximilian.marx@tu-dresden.de>
2017-08-01 20:22:22 +0200

    Add a progress bar based on angular-loading-bar

--- src/app/main.js
+++ src/app/main.js
@@ -5,7 +5,7 @@ requirejs.config({
 		"jquery": "../lib/jquery",
 		"jquery-ui": "../lib/jquery-ui",
 		"bootstrap": '../lib/bootstrap-3.3.6-dist/js/bootstrap',
-		
+
 		"spin": "../lib/spin",
 
 		"angular": "../lib/angular",
@@ -18,8 +18,8 @@ requirejs.config({
 		"ngTranslate-storage-loc": "../lib/angular-translate-storage-local",
 		"ngComplete": "../lib/angucomplete-alt",
 		"ui-boostrap-tpls": "../lib/ui-bootstrap-tpls-1.3.2",
-		"openLayers": "../lib/openlayers/OpenLayers"
-	
+		"openLayers": "../lib/openlayers/OpenLayers",
+        "angular-loading-bar": "../lib/loading-bar.min"
 	},
 	shim: {
 		'jquery-ui': ['jquery'],
@@ -33,18 +33,19 @@ requirejs.config({
 		'ngAnimate': ['angular'],
 		'ngRoute': ['angular'],
 		'ngCookies': ['angular'],
-		'ngTranslate-core': ['angular'], 
+		'ngTranslate-core': ['angular'],
 		'ngTranslate-loader': ['ngTranslate-core'],
 		'ngTranslate-storage-cook': ['ngTranslate-core', 'ngCookies'],
 		'ngTranslate-storage-loc': ['ngTranslate-storage-cook'],
-		'ngComplete': ['angular']
+		'ngComplete': ['angular'],
+        'angular-loading-bar': ['ngAnimate']
 	}
 });
 
 
-// Load everything, start the app 
-requirejs([ 'sqid.module' ], function() {
+// Load everything, start the app
+requirejs([ 'sqid.module', 'angular-loading-bar'], function() {
 	jQuery(function() {
-		angular.bootstrap( document, ['sqid'], { strictDi: true } );
+		angular.bootstrap( document, ['sqid', 'angular-loading-bar'], { strictDi: true } );
 	});
 });
