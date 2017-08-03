c29b1f4dea477cde694f5142db2ef9db72bae852
Maximilian Marx <maximilian.marx@tu-dresden.de>
2017-08-03 20:01:50 +0200

    Add rule browser

---
+++ src/app/rules/browse.controller.js
@@ -0,0 +1,18 @@
+define(['rules/rules.module',
+        'rules/provider.service'
+       ],
+function() {
+    angular.module('rules').controller('BrowseController',
+    ['$scope', '$translate',
+    'provider',
+    function($scope, $translate, provider) {
+        $scope.translation = {};
+        $scope.rules = provider
+            .getRules()
+            .map(function(rule) {
+                return angular.extend({}, rule);
+            });
+    }]);
+
+    return {};
+});
--- src/app/rules/rules.config.js
+++ src/app/rules/rules.config.js
@@ -6,6 +6,7 @@ define(['rules/rules.module',
         'rules/provider.service',
         'rules/references.service',
         'rules/instantiator.service',
+        'rules/browse.controller',
         'rules/explain.controller'
        ],
 function() {
