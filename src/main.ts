328d2a66bf8f98146caf24e0ca9eee0404c9f102
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2019-02-23 23:49:52 +0100

    Fix tslint warnings

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,17 @@
 import '@babel/polyfill'
-import Vue from 'vue';
+import Vue from 'vue'
 import './plugins/bootstrap-vue'
-import App from './App.vue';
-import router from './router';
-import store from './store';
-import './registerServiceWorker';
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import './registerServiceWorker'
 import i18n from './i18n'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
   i18n,
-  render: (h) => h(App)
-}).$mount('#app');
+  render: (h) => h(App),
+}).$mount('#app')
