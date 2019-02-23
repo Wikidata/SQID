480d0eb2a2d2c4537ba3869f8c32a58f6c15cef7
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2019-02-24 00:04:46 +0100

    Fix even more tslint warnings

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -33,11 +33,11 @@
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
+import { Component, Prop, Vue } from 'vue-property-decorator'
 
 @Component
 export default class HelloWorld extends Vue {
-  @Prop() private msg!: string;
+  @Prop() private msg!: string
 }
 </script>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,8 +6,8 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { Component, Vue } from 'vue-property-decorator'
+import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
 
 @Component({
   components: {
