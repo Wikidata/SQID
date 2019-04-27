153b24d3f435d923e24b4ad4564fa69098d4f8ea
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2019-04-27 23:24:59 +0200

    Factor out endpoint URI definitions

---
+++ src/api/endpoints.ts
@@ -0,0 +1,3 @@
+export const commonsEndpoint = 'https://commons.wikimedia.org/w/api.php'
+export const wikidataEndpoint = 'https://www.wikidata.org/w/api.php'
+export const sparqlEnpdoint = 'https://query.wikidata.org/bigdata/namespace/wdq/sparql'
