6f35f63889aaa834c3b7cf10c3ac26bf1649cd58
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2019-04-28 19:29:17 +0200

    Fix alias/description display

--- src/components/Entity.vue
+++ src/components/Entity.vue
@@ -1,9 +1,15 @@
 <template>
-  <div>
-    <h1><span>{{ label }}</span><small> (<a :href="wikidataUrl">{{ entityId }}</a>)</small>
-    </h1>
-    <ul><li v-for="alias in aliases">{{ alias }}</li></ul>
-    <span>{{ description }}</span>
+  <div class="container">
+    <div class="mainbar col-lg-9 col-md-12 col-sm-12">
+      <h1><span>{{ label }}</span>
+        &nbsp;<small>(<a :href="wikidataUrl">{{ entityId }}</a>)</small></h1>
+      <div id="aliases">
+        <ul class="list-inline">
+          <li class="list-inline-item" v-for="alias in aliases" :key="alias.value">{{ alias }}</li>
+        </ul>
+      </div>
+      <div id="description">{{ description }}</div>
+    </div>
   </div>
 </template>
 
@@ -21,6 +27,7 @@ export default class Entity extends Vue {
   private description: string | null = null
   private entityData: any | null = null
 
+  // todo(mx): retrieve canonical URI from entity data
   private get wikidataUrl() {
     return `https://www.wikidata.org/entity/${this.entityId}`
   }
@@ -46,3 +53,33 @@ export default class Entity extends Vue {
   }
 }
 </script>
+
+<style lang="less">
+  #aliases {
+    margin-top: -10px;
+    margin-bottom: 0px;
+    color: #999999;
+  }
+
+  #aliases ul {
+    margin-top: -10px;
+    margin-bottom: 0px;
+  }
+
+  #aliases ul li {
+    margin-top: -10px;
+    padding-right: 0px;
+  }
+
+  #aliases ul li::after {
+    content: " | ";
+  }
+
+  #aliases ul li:last-child::after {
+    content: "";
+  }
+
+  #description {
+    font-size: 1.3em;
+  }
+</style>
