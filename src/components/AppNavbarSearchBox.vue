bd765cf0a2a83651ddb75388d75d1f0e76ae39b3
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2019-08-10 21:38:25 +0200

    Workaround a limitation in vue-bootstrap-typeahead to search by QId

--- src/components/AppNavbarSearchBox.vue
+++ src/components/AppNavbarSearchBox.vue
@@ -1,23 +1,23 @@
 <template>
-<vue-bootstrap-typeahead
-  v-model="entitySearch"
-  :data="entities"
-  :serializer="s => s.label"
-  @hit="onSelectedEntity($event)"
-  size="sm"
-  :placeholder="$t('pageTitle.searchItem')"
-  :aria-label="$t('pageTitle.search')">
-  <template slot="append">
-    <b-button
-      size="sm"
-      @click="onEntitySearch(entitySearch)"><font-awesome-icon icon="search" /></b-button>
-  </template>
+  <vue-bootstrap-typeahead
+    v-model="entitySearch"
+    :data="entities"
+    :serializer="s => `${s.id}: : ${s.label} ( (${s.description}) )`"
+    @hit="onSelectedEntity($event)"
+    size="sm"
+    :placeholder="$t('pageTitle.searchItem')"
+    :aria-label="$t('pageTitle.search')"
+    ref="navbar-search-box">
+    <template slot="append">
+      <b-button
+        size="sm"
+        @click="onEntitySearch(entitySearch)"><font-awesome-icon icon="search" /></b-button>
+    </template>
 
-  <template slot="suggestion" slot-scope="{ data, htmlText }">
-    <div><span v-html="htmlText"></span>&nbsp;<small>({{ data.id }})</small>
-      <br /><small>{{ data.description }}</small></div>
-  </template>
-</vue-bootstrap-typeahead>
+    <template slot="suggestion" slot-scope="{ data, htmlText }">
+      <div><span v-html="workAroundFilter(htmlText)" /></div>
+    </template>
+  </vue-bootstrap-typeahead>
 </template>
 
 <script lang="ts">
@@ -27,6 +27,8 @@ import _ from 'lodash'
 
 import router from '@/router'
 import { searchEntities } from '@/api/wikidata'
+
+const TYPEAHEAD_WORKAROUND_REGEX = /^(.*): : (.*) \( \((.*)\) \)$/
 
 @Component({
   components: {
@@ -39,7 +41,6 @@ export default class AppNavbarSearchBox extends Vue {
   private searchEntities = _.debounce(async function(this: AppNavbarSearchBox, search) {
     try {
       const response = await searchEntities(search, { limit: 10 } )
-
       this.entities = []
       for (const [key, entity] of Object.entries(response)) {
         this.entities.push(entity)
@@ -50,6 +51,15 @@ export default class AppNavbarSearchBox extends Vue {
 
   }, 500)
 
+  private workAroundFilter(htmlText: string) {
+    // vue-bootstrap-typeahead filters results on the serialised values
+    // since we don't want to, e.g., exclude Q42 from the results for Q42
+    // simply because `Q42' does not appear in the label, so we shuffle
+    // things around manually
+    return htmlText.replace(TYPEAHEAD_WORKAROUND_REGEX,
+                            '$2 <small>$1</small><br /><small>$3</small>').trim()
+  }
+
   @Watch('entitySearch', {})
   private onEntitySearch(search: any) {
     this.searchEntities(search)
@@ -59,6 +69,8 @@ export default class AppNavbarSearchBox extends Vue {
     router.push({ name: 'entity',
                   params: { id: event.id },
                 })
+    const searchBox: { inputValue: string } = this.$refs['navbar-search-box'] as any
+    searchBox.inputValue = searchBox.inputValue.replace(TYPEAHEAD_WORKAROUND_REGEX, '$2').trim()
   }
 }
 </script>
