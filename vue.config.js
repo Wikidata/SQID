d92b54d2451bf838e141cf771120ee8ae6674913
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2019-08-11 21:09:43 +0200

    Set publicPath for production builds

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  publicPath: (process.env.NODE_ENV === 'production'
+               ? '/sqid/sqid-ng-test/'
+               : '/'),
   pluginOptions: {
     i18n: {
       locale: 'en',
