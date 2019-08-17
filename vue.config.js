78f721324595920b639795760f3e4c5d481d4c67
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2019-08-17 11:52:02 +0200

    Update build path

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   publicPath: (process.env.NODE_ENV === 'production'
-               ? '/sqid/sqid-ng-test/'
+               ? '/sqid/sqid-ng/'
                : '/'),
   pluginOptions: {
     i18n: {
