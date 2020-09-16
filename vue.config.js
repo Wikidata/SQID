5494eccaa49a908484eccfdee50e60f7f987d189
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2020-09-16 11:48:53 +0200

    Fix publicPath for toolforge.org deployment

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   publicPath: (process.env.NODE_ENV === 'production'
-               ? '/sqid/sqid-ng/'
+               ? '/sqid-ng/'
                : '/'),
   pluginOptions: {
     i18n: {
