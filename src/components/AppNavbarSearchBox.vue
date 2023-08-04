bc3c936808a787a84c25286c8d15b6a4c4ace96c
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2023-08-04 23:56:45 +0200

    Move to vue-bootstrap-autocomplete

--- src/components/AppNavbarSearchBox.vue
+++ src/components/AppNavbarSearchBox.vue
@@ -1,5 +1,5 @@
 <template>
-  <vue-bootstrap-typeahead
+  <vue-bootstrap-autocomplete
     v-model="entitySearch"
     :data="entities"
     :serializer="s => `${s.id}: : ${s.label || ''} ( (${s.description || ''}) )`"
@@ -7,81 +7,91 @@
     size="sm"
     :placeholder="$t('pageTitle.searchItem')"
     :aria-label="$t('pageTitle.search')"
-    ref="navbar-search-box">
+    ref="navbar-search-box"
+  >
     <template slot="append">
-      <b-button
-        size="sm"
-        @click="onEntitySearch(entitySearch)"><font-awesome-icon icon="search" /></b-button>
+      <b-button size="sm" @click="onEntitySearch(entitySearch)"
+        ><font-awesome-icon icon="search"
+      /></b-button>
     </template>
 
     <template slot="suggestion" slot-scope="{ data, htmlText }">
       <div><span v-html="workAroundFilter(htmlText)" /></div>
     </template>
-  </vue-bootstrap-typeahead>
+  </vue-bootstrap-autocomplete>
 </template>
 
 <script lang="ts">
-import { Component, Model, Watch, Vue } from 'vue-property-decorator'
-import VueBootstrapTypeahead from 'vue-bootstrap-typeahead'
-import _ from 'lodash'
+  import { Component, Model, Watch, Vue } from 'vue-property-decorator'
+  import VueBootstrapAutocomplete from '@vue-bootstrap-components/vue-bootstrap-autocomplete'
+  import _ from 'lodash'
 
-import router from '@/router'
-import { searchEntities } from '@/api/wikidata'
+  import router from '@/router'
+  import { searchEntities } from '@/api/wikidata'
 
-const TYPEAHEAD_WORKAROUND_REGEX = /^(.*): : (.*) \( \((.*)\) \)$/
+  const TYPEAHEAD_WORKAROUND_REGEX = /^(.*): : (.*) \( \((.*)\) \)$/
 
-@Component({
-  components: {
-    VueBootstrapTypeahead,
-  },
-})
-export default class AppNavbarSearchBox extends Vue {
-  private entitySearch: string = ''
-  private entities: any[] = []
-  private searchEntities = _.debounce(async function(this: AppNavbarSearchBox, search) {
-    try {
-      const response = await searchEntities(search, { limit: 10 })
-      this.entities = []
-      for (const [key, entity] of Object.entries(response)) {
-        this.entities.push(entity)
-      }
-
-      if (search.match(/^P\d+$/)) {
-        const properties = await searchEntities(search, { limit: 10,
-                                                          kind: 'property',
-                                                        })
-
-        for (const [key, entity] of Object.entries(properties)) {
+  @Component({
+    components: {
+      VueBootstrapAutocomplete,
+    },
+  })
+  export default class AppNavbarSearchBox extends Vue {
+    private entitySearch: string = ''
+    private entities: any[] = []
+    private searchEntities = _.debounce(async function (
+      this: AppNavbarSearchBox,
+      search,
+    ) {
+      try {
+        const response = await searchEntities(search, { limit: 10 })
+        this.entities = []
+        for (const [key, entity] of Object.entries(response)) {
           this.entities.push(entity)
         }
-      }
 
-    } catch (err) {
-      // do nothing
+        if (search.match(/^P\d+$/)) {
+          const properties = await searchEntities(search, {
+            limit: 10,
+            kind: 'property',
+          })
+
+          for (const [key, entity] of Object.entries(properties)) {
+            this.entities.push(entity)
+          }
+        }
+      } catch (err) {
+        // do nothing
+      }
+    }, 500)
+
+    private workAroundFilter(htmlText: string) {
+      // vue-bootstrap-typeahead filters results on the serialised values
+      // since we don't want to, e.g., exclude Q42 from the results for Q42
+      // simply because `Q42' does not appear in the label, so we shuffle
+      // things around manually
+      // todo(mx): figure out if this is still required
+      return htmlText
+        .replace(
+          TYPEAHEAD_WORKAROUND_REGEX,
+          '$2 <small>$1</small><br /><small>$3</small>',
+        )
+        .trim()
     }
 
-  }, 500)
+    @Watch('entitySearch', {})
+    private onEntitySearch(search: any) {
+      this.searchEntities(search)
+    }
 
-  private workAroundFilter(htmlText: string) {
-    // vue-bootstrap-typeahead filters results on the serialised values
-    // since we don't want to, e.g., exclude Q42 from the results for Q42
-    // simply because `Q42' does not appear in the label, so we shuffle
-    // things around manually
-    return htmlText.replace(TYPEAHEAD_WORKAROUND_REGEX,
-                            '$2 <small>$1</small><br /><small>$3</small>').trim()
+    private onSelectedEntity(event: any) {
+      router.push({ name: 'entity', params: { id: event.id } })
+      const searchBox: { inputValue: string } = this.$refs[
+        'navbar-search-box'
+      ] as any
+      searchBox.inputValue = searchBox.inputValue
+        .replace(TYPEAHEAD_WORKAROUND_REGEX, '$2')
+        .trim()
+    }
   }
-
-  @Watch('entitySearch', {})
-  private onEntitySearch(search: any) {
-    this.searchEntities(search)
-  }
-
-  private onSelectedEntity(event: any) {
-    router.push({ name: 'entity',
-                  params: { id: event.id },
-                })
-    const searchBox: { inputValue: string } = this.$refs['navbar-search-box'] as any
-    searchBox.inputValue = searchBox.inputValue.replace(TYPEAHEAD_WORKAROUND_REGEX, '$2').trim()
-  }
-}
 </script>
