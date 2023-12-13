bd20cf7a190690985cb67c07ab9751172a4dc4b2
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2023-12-13 22:11:55 +0100

    Pass dump date in correct format

--- helpers/rust/src/jobs.rs
+++ helpers/rust/src/jobs.rs
@@ -6,14 +6,9 @@ use chrono::NaiveDate;
 pub(super) fn do_dump_processing(dump_file: &str, dump_date: &NaiveDate) -> Result<()> {
     let sqid = current_exe().context("failed to get executable path")?;
     let sqid = sqid.to_str().expect("should be valid UTF-8");
+    let dump_date = dump_date.format("%Y%m%d").to_string();
     let mut command = Command::new(sqid);
-    command.args([
-        "--only=process-dump",
-        "--date",
-        &format!("{dump_date}"),
-        "--",
-        dump_file,
-    ]);
+    command.args(["--only=process-dump", "--date", &dump_date, "--", dump_file]);
 
     log::info!("executing: {command:?}");
 
